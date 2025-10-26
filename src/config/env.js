6912abf4345e3a1458aaa8f9eecae75314475866
Joao Vitor Rodrigues da Silva <joao.vitor.r.silva.3@gmail.com>
2025-10-26 10:59:23 -0300

    added api url to env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,7 @@
-import "dotenv/config"
+import "dotenv/config";
 export const ENV = {
-    PORT : process.env.PORT,
-    DATABASE_URL: process.env.DATABASE_URL,
-    NODE_ENV: process.env.NODE_ENV,
-}
+  PORT: process.env.PORT,
+  DATABASE_URL: process.env.DATABASE_URL,
+  NODE_ENV: process.env.NODE_ENV,
+  API_URL: process.env.API_URL,
+};
